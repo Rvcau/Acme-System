29b8b31f5088389e9147b068abdcc795d13cb0c3
Riley Vicente <rvicente0725@gmail.com>
2025-08-20 16:11:52 -0400

    Added heyNow method

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -5,4 +5,11 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.out.println("Riley is working locally on his branch");
     }
+    public static void heyNow() {
+
+        String Msg = "Hey Now!";
+
+        System.out.println(Msg);
+
+    }
 }
